e8cf4660ae1de4325d6866ce5da19bc7cb22f5da
Geniux <47742379+theevilgeniux@users.noreply.github.com>
2020-09-09 02:39:28 +0100

    new portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// $(document).mouseup(function(e) {
+//     var container = $('#menubar');
+//     if (!container.is(e.target) && container.has(e.target).length === 0) {
+//         document.getElementById('menubar').style.cssText="transform: translateX(-115%);";
+//     }
+
+// });
