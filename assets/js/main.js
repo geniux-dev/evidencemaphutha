07d3e57d59f2f9e9e45ddcd2dd8479646c38a3ad
Geniux <47742379+theevilgeniux@users.noreply.github.com>
2020-09-12 00:10:06 +0100

    new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,51 @@
 //     }
 
 // });
+
+function view_tab(d){
+    $('#profileTab').removeClass('active');
+    $('#projectTab').removeClass('active');
+    $('#cvTab').removeClass('active');
+    if(d=="profile"){
+        $('.contents-center').html('<div class="loader-p"><div class="ld ld-ring ld-spin"></div></div>');
+        var delayInMilliseconds = 700;  
+            setTimeout(function() {
+            $('.contents-center').load('contents/profile.html');
+        }, delayInMilliseconds);
+    }
+    if(d=="projects"){
+        $('.contents-center').html('<div class="loader-p"><div class="ld ld-ring ld-spin"></div> </div>');
+        var delayInMilliseconds = 700;  
+            setTimeout(function() {
+            $('.contents-center').load('contents/projects.html');
+        }, delayInMilliseconds);
+    }
+    if(d=="cv"){
+        $('.contents-center').html('<div class="loader-p"><div class="ld ld-ring ld-spin"></div> </div>');
+        var delayInMilliseconds = 700;  
+            setTimeout(function() {
+            $('.contents-center').load('contents/cv.html');
+        }, delayInMilliseconds);
+    }
+}
+
+function menutoggle(){
+    var menubar = document.getElementById('menubar');
+    if(menubar.style.cssText=="transform: translateX(0%);" || menubar.style.cssText==""){
+        menubar.style.cssText = "transform: translateX(-115%);"
+
+    }else{
+        menubar.style.cssText = "transform: translateX(0%);";
+   
+    }
+}
+$(document).ready(function(){
+    view_tab('profile');
+});
+$(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $('.menu-btn').addClass('menu-bg');
+    } else {
+        $('.menu-btn').removeClass('menu-bg');
+    }
+}); 
